5dc7143b705c0ec6cc39977088d6b9e7d123b07b
zachflanders <zachflanders@gmail.com>
2019-03-07 07:54:48 -0600

    one more time

--- server.js
+++ server.js
@@ -44,7 +44,9 @@ sequelize
   });
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
